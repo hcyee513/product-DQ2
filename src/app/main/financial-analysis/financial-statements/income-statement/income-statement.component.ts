a72d61c4da925f8bce774d5084cd391ea60c7cb9
hcyee <angeliyou0513@gmail.com>
2024-05-17 16:10:17 +0800

    綜合損益表切版

--- src/app/main/financial-analysis/financial-statements/income-statement/income-statement.component.ts
+++ src/app/main/financial-analysis/financial-statements/income-statement/income-statement.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class IncomeStatementComponent implements OnInit {
 
   public selectedRadio: string = 'yearData';
+  // 初始顯示table
+  public slideToggle: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public changeSlideToggle(event: any):void {
+    this.slideToggle = event.target.checked;
   }
 
 }
